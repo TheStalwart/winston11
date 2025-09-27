eab668100a02c05ff885d60b036cf3f81d36cdca
Pavel Shevchuk <stlwrt@gmail.com>
2025-09-27 03:54:42 +0300

    Improved Install-EssentialDiagnosticApps function with doc comments and specific package ID enforcement in winget command

--- Winston.ps1
+++ Winston.ps1
@@ -1,8 +1,14 @@
+<#
+.SYNOPSIS
+  Installs essential diagnostic apps.
+.DESCRIPTION
+  If winget finds any packages out of date, it will update them.
+#>
 function Install-EssentialDiagnosticApps {
-    winget install `
-        REALiX.HWiNFO `
-        CrystalDewWorld.CrystalDiskInfo `
-        CrystalDewWorld.CrystalDiskMark
+    winget install --id REALiX.HWiNFO -e
+    winget install --id CrystalDewWorld.CrystalDiskInfo -e
+    winget install --id CrystalDewWorld.CrystalDiskMark -e
+}
 }
 
 function Main {
