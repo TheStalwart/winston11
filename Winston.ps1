0b897d078c2f8f3c7d93fd7f4fa2dca2b73cd48b
Pavel Shevchuk <stlwrt@gmail.com>
2025-10-06 00:19:23 +0300

    Added Speedtest app to Install-EssentialUtilities

--- Winston.ps1
+++ Winston.ps1
@@ -45,6 +45,7 @@ function Install-EssentialUtilities {
     winget install --id Guru3D.RTSS -e # Standalone frame rate limiter
     winget install --id PrimateLabs.Geekbench.6 -e # OpenCL and Vulkan benchmark
     winget install --id Unigine.HeavenBenchmark -e # DirectX 9 and 11 benchmark
+    winget install --id Ookla.Speedtest.Desktop -e
 }
 
 function Install-WinCaffeine {
